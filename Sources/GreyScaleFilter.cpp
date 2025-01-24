d62de6bf10ef27fc40de5cf86112b623d268e64d
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-01-24 10:21:26 +0100

    Feat : grey scale

--- Sources/GreyScaleFilter.cpp
+++ Sources/GreyScaleFilter.cpp
@@ -9,13 +9,13 @@ GreyScaleFilter::~GreyScaleFilter() {
 }
 
 Mat GreyScaleFilter::applyFilter(const Mat& inputFrame) {
-    // if (inputFrame.empty()) {
-    //     cerr << "Error: Empty input frame provided to GreyScaleFilter." << endl;
-    //     return;
-    // }
+    if (inputFrame.empty()) {
+        cerr << "Error: Empty input frame provided to GreyScaleFilter." << endl;
+        return Mat();
+    }
 
     Mat grayFrame;
-    cvtColor(inputFrame, grayFrame, COLOR_BGRA2RGBA); // Convert to greyscale COLOR_BGRA2RGBA COLOR_RGB2HSV_FULL COLOR_HLS2BGR
+    cvtColor(inputFrame, grayFrame, COLOR_BGR2GRAY); // Convert to greyscale
 
     imshow(windowName, grayFrame); // Display the greyscale frame
     return grayFrame; // Return the processed greyscale frame for further use
