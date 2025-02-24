37f49e47dc60b48935302db5252cb4fb7e8537db
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-02-24 11:20:08 +0100

    [feat] Resize rotate filter

---
+++ Headers/ResizeRotateFilter.hpp
@@ -0,0 +1,30 @@
+#ifndef RESIZEROTATEFILTER_HPP
+#define RESIZEROTATEFILTER_HPP
+
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+class ResizeRotateFilter {
+public:
+    ResizeRotateFilter(double scale = 1.0, double angle = 0.0);
+    ~ResizeRotateFilter();
+
+    void setScale(double scale);
+    void setAngle(double angle);
+    double getScale() const;
+    double getAngle() const;
+
+    Mat applyFilter(const Mat& inputFrame);
+
+    string getWindowName() const { return windowName; }
+
+private:
+    double scale;
+    double angle;
+    string windowName = "Resize & Rotate Filter";
+};
+
+#endif // RESIZEROTATEFILTER_HPP
---
+++ Sources/ResizeRotateFilter.cpp
@@ -0,0 +1,48 @@
+#include "Headers/ResizeRotateFilter.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+ResizeRotateFilter::ResizeRotateFilter(double scale, double angle)
+    : scale(scale), angle(angle) {
+}
+
+ResizeRotateFilter::~ResizeRotateFilter() {
+    destroyWindow(windowName);
+}
+
+void ResizeRotateFilter::setScale(double scale) {
+    this->scale = scale;
+}
+
+void ResizeRotateFilter::setAngle(double angle) {
+    this->angle = angle;
+}
+
+double ResizeRotateFilter::getScale() const {
+    return scale;
+}
+
+double ResizeRotateFilter::getAngle() const {
+    return angle;
+}
+
+Mat ResizeRotateFilter::applyFilter(const Mat& inputFrame) {
+    if (inputFrame.empty()) {
+        cerr << "Error: Empty input frame provided to ResizeRotateFilter." << endl;
+        return Mat();
+    }
+    
+    // Resize the image
+    Mat resized;
+    resize(inputFrame, resized, Size(), scale, scale);
+
+    // Rotate the image
+    Point2f center(resized.cols / 2.0F, resized.rows / 2.0F);
+    Mat rotMat = getRotationMatrix2D(center, angle, 1.0);
+    Mat rotated;
+    warpAffine(resized, rotated, rotMat, resized.size());
+
+    return rotated;
+}
